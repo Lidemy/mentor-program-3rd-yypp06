8dfa8233809e3295d8357cb4e140036f275c65b3
Neo <yypp06@gmail.com>
2019-04-30 14:52:23 +0800

    hw1

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,13 @@
 function stars(n) {
-  console.log(n);
+  for (let i = 1; i <= n; i + 1) {
+    let newStar = '';
+    for (let j = 1; j <= i; j + 1) {
+      newStar += '*';
+    }
+    const starArray = newStar.split(',');
+    console.log(starArray);
+  }
 }
+stars(3);
 
 module.exports = stars;
