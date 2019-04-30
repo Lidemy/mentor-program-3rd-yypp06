7d047f6e0ebd97cf68c158648ce7406e1c741e7d
Neo <yypp06@gmail.com>
2019-04-30 12:24:06 +0800

    fix

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,15 @@
 function capitalize(str) {
-  return str;
+  let result = '';
+  for (let i = 0; i < str.length; i + 1) {
+    if (i === 0) {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
-console.log(capitalize('hello'));
+console.log(capitalize('nick'));
+console.log(capitalize(',nick'));
+console.log(capitalize('Nick'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,18 @@
 function join(str, concatStr) {
-  return concatStr;
+  let newStr = str[0];
+  for (let i = 1; i < str.length; i + 1) {
+    newStr += concatStr + str[i];
+  }
+  return newStr;
 }
 
 function repeat(str, times) {
-  return times;
+  let newStr = str[0];
+  for (let i = 0; i < times; i + 1) {
+    newStr += str;
+  }
+  return newStr;
 }
 
-console.log(join('a', '!'));
-console.log(repeat('a', 5));
+console.log(join(['a', 'b', 'c'], '!'));
+console.log(repeat('yo', 5));
