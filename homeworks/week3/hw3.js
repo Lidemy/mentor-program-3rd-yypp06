1294e19faedc901be819b4a86a7ad725f0b21816
Neo <yypp06@gmail.com>
2019-04-30 16:04:38 +0800

    hw3

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,16 @@
 function isPrime(n) {
-  console.log(n);
+  if (n === 1) {
+    return false;
+  }
+  for (let i = 2; i <= n - 1; i + 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
+console.log(isPrime(39));
+console.log(isPrime(37));
+console.log(isPrime(22));
 
 module.exports = isPrime;
