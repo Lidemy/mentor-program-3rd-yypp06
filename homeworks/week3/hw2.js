1f69ce978b1603279540f5b66c560834ce10172d
Neo <yypp06@gmail.com>
2019-04-30 15:04:01 +0800

    hw2

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,16 @@
 function alphaSwap(str) {
-  console.log(str);
+  let newstr = '';
+  for (let i = 0; i <= str.length; i + 1) {
+    if (str[i] >= 'a' && str <= 'z') {
+      newstr += str[i].toUpperCase();
+    } else if (str[i] >= 'A' && str <= 'Z') {
+      newstr += str[i].toLowerCase();
+    } else {
+      newstr += str[i];
+    }
+  }
+  return newstr;
 }
+console.log(alphaSwap('Hellow'));
 
 module.exports = alphaSwap;
