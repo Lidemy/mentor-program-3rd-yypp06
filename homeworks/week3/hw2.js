a0c840de9b1ab04fd6aef76904423c1fcf0d32a4
Neo <yypp06@gmail.com>
2019-04-30 15:37:49 +0800

    fix

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,6 +1,6 @@
 function alphaSwap(str) {
   let newstr = '';
-  for (let i = 0; i <= str.length; i + 1) {
+  for (let i = 0; i < str.length; i + 1) {
     if (str[i] >= 'a' && str <= 'z') {
       newstr += str[i].toUpperCase();
     } else if (str[i] >= 'A' && str <= 'Z') {
@@ -11,6 +11,6 @@ function alphaSwap(str) {
   }
   return newstr;
 }
-console.log(alphaSwap('Hellow'));
+console.log(alphaSwap('Hello'));
 
 module.exports = alphaSwap;
