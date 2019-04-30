62d2469bf2f0926452eea217d7cb47a76cca0a79
Neo <yypp06@gmail.com>
2019-04-30 16:24:45 +0800

    hw4

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,17 @@
 function isPalindromes(str) {
-  console.log(str);
+  const splitString = str.split('');
+  const reverseStr = splitString.reverse('');
+  const joinString = reverseStr.join('');
+
+  if (str === joinString) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 }
+isPalindromes('111111');
+isPalindromes('lalalal');
+isPalindromes('543212345');
+isPalindromes('lslsls');
 
 module.exports = isPalindromes;
